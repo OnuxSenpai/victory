3402f0839eb764bf3da396ae4a7c2e72c6a44c85
OnuxSenpai <asasolnusko22210@gmail.com>
2025-06-11 00:50:57 +0300

    Add files via upload

---
+++ proba2/proba2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Numbers
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        // 1 не является простым числом
+
+    }
+}
